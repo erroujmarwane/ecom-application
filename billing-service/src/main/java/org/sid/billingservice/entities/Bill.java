ddeb08566b58f4f5340f033be8c2fc0062708868
Marouane Erroujdani <erroujmarwane@gmail.com>
2022-12-16 16:15:16 +0100

    version1.0

---
+++ billing-service/src/main/java/org/sid/billingservice/entities/Bill.java
@@ -0,0 +1,31 @@
+package org.sid.billingservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.billingservice.model.Customer;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Bill {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date billDate;
+    private Long customerId;
+    @OneToMany(mappedBy = "bill")
+    private List<ProductItem> productItems;
+    @Transient
+    private Customer customer;
+
+    public double getTotal(){
+        double sum=0;
+        for (ProductItem pi:productItems){
+            sum+=pi.getAmount();
+        }
+        return sum;
+    }
+}
